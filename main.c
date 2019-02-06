f5290b67b3f03eea22695604d100d761208bf9ba
Steven Cipriano <redsox333@gmail.com>
2019-02-06 02:47:18 -0800

    calculate tag size

--- main.c
+++ main.c
@@ -6,9 +6,11 @@ FILE *fptr;
 void print_hex(unsigned char *, int, int);
 void print_chars(unsigned char *, int, int);
 void print_binary(unsigned char *, int, int);
+unsigned long int get_tag_size(unsigned char *);
 
 int main() {
     unsigned char *header;
+    unsigned long int tag_size;
 
     fptr = fopen("data/soviet_anthem.mp3", "rb");
     header = (unsigned char *)malloc(10 * sizeof(unsigned char));
@@ -31,9 +33,11 @@ int main() {
     print_binary(header, 5, 1);
 
     // size (4)
-     printf("SIZE:\n");
-     print_hex(header, 6, 4);
-     print_binary(header, 6, 4);
+    printf("SIZE:\n");
+    print_hex(header, 6, 4);
+    print_binary(header, 6, 4);
+    tag_size = get_tag_size(header);
+    printf("tag size: %lu bytes\n", tag_size);
 
     return 0;
 }
@@ -79,4 +83,31 @@ void print_binary(unsigned char * data, int start, int num) {
         printf("%s ", bits);
     }
     printf("\n");
+}
+
+unsigned long int get_tag_size(unsigned char * header) {
+    // start from last size byte, moving backwards
+    // and tracking the exponent. The bytes are big-endian
+
+    unsigned long int total = 0;
+    unsigned char byte;
+    int i, j;
+    int exponent = 0;
+
+    for (i = 9; i > 5; i--) {
+        byte = header[i];
+
+        // ignore the last bit, (it's always set to 0)
+        for (j = 0; j < 7; j++, exponent++) {
+            if (byte & 1) {
+                total += 1 << exponent;    // exponent of 2
+            }
+            byte>>=1;
+        }
+    }
+
+    // add the 10 bytes for header itself
+    total += 10;
+
+    return total;
 }
